dfb6220b69eaab300cdb69554cd2e91b05e810ca
jburto94 <jburto94@yahoo.com>
2018-05-05 15:05:28 -0400

    Added proptypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux',
@@ -161,6 +162,13 @@ const Search = ({
     </button>
   </form>
 
+Search.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired
+}
+
 const Table = ({list, onDismiss}) =>
   <div className='table'>
     {list.map(item => 
@@ -188,13 +196,27 @@ const Table = ({list, onDismiss}) =>
     )}
   </div>
 
-const Button = ({onClick, className, children}) =>
+Table.propTypes = {
+  list: PropTypes.array.isRequired,
+  onDismiss: PropTypes.func.isRequired
+};
+
+const Button = ({onClick, 
+                 className, 
+                 children
+}) =>
   <button 
     onClick={onClick} 
     className={className} 
     type='button' >
     {children}
   </button>
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+};
 
 export default App;
 
