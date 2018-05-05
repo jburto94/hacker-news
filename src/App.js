29dae2415c81c654e922831332eb2b7b88eb41e1
jburto94 <jburto94@yahoo.com>
2018-05-05 14:44:43 -0400

    Added snapshot and unit tests

--- src/App.js
+++ src/App.js
@@ -181,7 +181,7 @@ const Table = ({list, onDismiss}) =>
           <Button 
             onClick={() => onDismiss(item.objectID)}
             className='button-inline'>
-            Dismiss
+            Remove
           </Button>
         </span>
       </div>
@@ -197,3 +197,9 @@ const Button = ({onClick, className, children}) =>
   </button>
 
 export default App;
+
+export {
+  Button,
+  Search,
+  Table
+};
